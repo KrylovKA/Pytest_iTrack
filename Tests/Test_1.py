bfd94cb721a2dc940b84cafe2e13e5e97b62980e
KrylovKA <krylovearth@gmail.com>
2020-03-19 18:01:38 +0300

    Local Add class TraceWay

--- Tests/Test_1.py
+++ Tests/Test_1.py
@@ -7,3 +7,12 @@ assert opts.headless  # без графического интерфейса.
 
 browser = Firefox(options=opts)
 browser.get('https://duckduckgo.com')
+
+
+class TraceWay(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('http://itrack.demo.dev-og.com/')
+        sleep(1)
+        assert "TraceWay" in driver.page_source
+        driver.quit()
